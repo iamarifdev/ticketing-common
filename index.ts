dd2f1db76cb8b3052ae4417296bb3eb12a5f0109
Md. Ariful Islam <arifjmamun24@gmail.com>
2022-09-16 17:37:42 +0600

    Updates

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export * from './src/errors';
 export * from './src/middlewares';
 export * from './src/events';
+export * from './src/events/types';
---
+++ src/events/types/order-status.ts
@@ -0,0 +1,26 @@
+export enum OrderStatus {
+  /**
+   * @description When the order has been created, but the ticket
+   * it is trying to order has not been reserved.
+   */
+  Created = 'created',
+
+  /**
+   * @description The ticket the order is trying to reserve has
+   * already been reserved, or when the user has cancelled the order.
+   * 
+   * The order expires before payment.
+   */
+  Cancelled = 'cancelled',
+
+  /**
+   * @description The order has successfully reserved the ticket.
+   */
+  AwaitingPayment = 'awaiting:payment',
+
+  /**
+   * @description The order has reserved the ticket and the user
+   * has provided payment successfully.
+   */
+  Complete = 'complete',
+}
