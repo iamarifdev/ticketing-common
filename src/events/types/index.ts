5b7c052b4e39d510b0624e1ad0175618a7f9ce78
Md. Ariful Islam <arifjmamun24@gmail.com>
2022-09-21 14:04:57 +0600

    Updates

--- src/events/types/index.ts
+++ src/events/types/index.ts
@@ -1,1 +1,2 @@
 export * from './order-status';
+export * from './queue-group';
---
+++ src/events/types/queue-group.ts
@@ -0,0 +1,6 @@
+export enum QueueGroup {
+  OrderService = 'order-service',
+  ExpirationService = 'expiration-service',
+  PaymentService = 'payment-service',
+  TicketService = 'ticket-service',
+}
